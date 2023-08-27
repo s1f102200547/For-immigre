e8ef42a1bb0eb59294416d1aaaf0c98e51d50f5a
s1f102200547 <s1f102200547@iniad.org>
2023-08-27 09:40:45 +0900

    create SecondMainContents-test

---
+++ src/routes/SignIn/SecondMainContents.test.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { render, fireEvent, screen, waitFor } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import SecondMainContents from './SecondMainContents';
+
+describe('SecondMainContents', () => {
+  const emailValue = 'test@test.com';
+  const passwordValue = 'test1234';
+
+  beforeEach(() => {
+    // Firebaseのモック設定
+    const mockSignInWithEmailAndPassword = jest.fn().mockResolvedValue({ user: { uid: 'testUserId' } });
+    jest.mock('../../firebase/firebase', () => ({
+      auth: () => ({
+        signInWithEmailAndPassword: mockSignInWithEmailAndPassword
+      })
+    }));
+  });
+
+  test('should fill out form and submit', async () => {
+    const mockChangeBlurredBackgroundColor = jest.fn();
+    const mockSetLoggedInUser = jest.fn();
+
+    render(
+      <MemoryRouter>
+        <SecondMainContents
+          changeBlurredBackgroundColor={mockChangeBlurredBackgroundColor}
+          SetLoggedInUser={mockSetLoggedInUser}
+        />
+      </MemoryRouter>
+    );
+
+    const emailInput = screen.getByTestId('email-input');
+    const passwordInput = screen.getByTestId('password-input');
+    const signinButton = screen.getByTestId('signin-button');
+
+    // フォームに入力
+    fireEvent.change(emailInput, { target: { value: emailValue } });
+    fireEvent.change(passwordInput, { target: { value: passwordValue } });
+
+    // サブミットボタンをクリック
+    fireEvent.click(signinButton);
+
+    // Firebaseの非同期操作を待つ
+    await waitFor(() => {
+      expect(mockChangeBlurredBackgroundColor).toHaveBeenCalledTimes(1);
+      expect(mockSetLoggedInUser).toHaveBeenCalledTimes(1);
+    });
+  });
+});
